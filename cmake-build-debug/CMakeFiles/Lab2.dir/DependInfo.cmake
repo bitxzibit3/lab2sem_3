a7d0ee16655b238ecf3b73020dfe792e6aef3641
bitxzibit3 <71255336+bitxzibit3@users.noreply.github.com>
2022-01-27 14:10:03 +0300

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/Lab2.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Timur/Labs_3semester/Lab2/Body.cpp" "C:/Users/Timur/Labs_3semester/Lab2/cmake-build-debug/CMakeFiles/Lab2.dir/Body.cpp.obj"
+  "C:/Users/Timur/Labs_3semester/Lab2/Knap.cpp" "C:/Users/Timur/Labs_3semester/Lab2/cmake-build-debug/CMakeFiles/Lab2.dir/Knap.cpp.obj"
+  "C:/Users/Timur/Labs_3semester/Lab2/Pair.cpp" "C:/Users/Timur/Labs_3semester/Lab2/cmake-build-debug/CMakeFiles/Lab2.dir/Pair.cpp.obj"
+  "C:/Users/Timur/Labs_3semester/Lab2/main.cpp" "C:/Users/Timur/Labs_3semester/Lab2/cmake-build-debug/CMakeFiles/Lab2.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Lab2.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab2.dir/Body.cpp.obj"
+  "CMakeFiles/Lab2.dir/Knap.cpp.obj"
+  "CMakeFiles/Lab2.dir/Pair.cpp.obj"
+  "CMakeFiles/Lab2.dir/main.cpp.obj"
+  "Lab2.exe"
+  "Lab2.exe.manifest"
+  "Lab2.pdb"
+  "libLab2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lab2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
